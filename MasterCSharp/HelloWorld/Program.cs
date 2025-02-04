c5a74238c13b205ff97eb0bbd7b2f49f3bd05b23
Alex Amedia <aamedia@asu.edu>
2025-02-04 09:03:42 -0700

    String Formatting and TryParse

--- MasterCSharp/HelloWorld/Program.cs
+++ MasterCSharp/HelloWorld/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Parse a number from a string
+//Console.WriteLine(int.Parse(Console.ReadLine()));
+
+// String formatting
+int num = 10;
+double price = 19.95;
+Console.WriteLine("The number is {0}, and the price is {1}", num, price);
+string myString = $"hello {price}";
+
+// TryParse
+int result;
+bool success = int.TryParse(Console.ReadLine(), out result);
+// if (success) { } ...
