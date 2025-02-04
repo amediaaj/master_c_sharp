52db6b2fd163d23bdaca333c2ff833d59704956a
Alex Amedia <aamedia@asu.edu>
2025-02-04 13:34:15 -0700

    Sort, Reverse

--- MasterCSharp/ListsApp/Program.cs
+++ MasterCSharp/ListsApp/Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             // Creating a list and initializing
-            List<string> colors = new List<string>();
-            colors.Add("red");
-            colors.Add("blue");
-            colors.Add("green");
-            colors.Add("red");
+            List<string> colors = ["red", "blue", "green", "red"];
 
-            foreach(string color in colors)
+            //List<string> colors = new List<string>
+            //{
+            //    "red",
+            //    "blue",
+            //    "green",
+            //    "red"
+            //};
+
+            foreach (string color in colors)
             {
                 Console.WriteLine(color);
             }
@@ -23,6 +27,21 @@
             foreach (string color in colors)
             {
                 Console.WriteLine(color);
+            }
+
+            // Array vs curly brace notation
+            List<int> numbers = [10, 5, 15, 3, 9];
+            //List<int> numbers = new List<int> { 10, 5, 15, 3, 9 };
+            numbers.Sort();
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine();
+            numbers.Reverse();
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
             }
         }
     }
