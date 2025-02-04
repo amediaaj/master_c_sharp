d4cfd0286cd83083415e19395c925b37c6c08749
Alex Amedia <aamedia@asu.edu>
2025-02-04 13:22:57 -0700

    Lists, Add, Remove

---
+++ MasterCSharp/ListsApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ListsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a list and initializing
+            List<string> colors = new List<string>();
+            colors.Add("red");
+            colors.Add("blue");
+            colors.Add("green");
+            colors.Add("red");
+
+            foreach(string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.WriteLine();
+
+            // Rmove() returns bool
+            Console.WriteLine(colors.Remove("red"));
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
