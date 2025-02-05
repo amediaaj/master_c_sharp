e30120f97f8247e1a41b929bb03b428219b004a6
Alex Amedia <aamedia@asu.edu>
2025-02-05 08:49:09 -0700

    Predicates with Find All and Delegates

--- MasterCSharp/ListsApp/Program.cs
+++ MasterCSharp/ListsApp/Program.cs
@@ -34,11 +34,20 @@
             List<int> numbers = [10, 5, 15, 3, 9, 25, 18];
             //List<int> numbers = new List<int> { 10, 5, 15, 3, 9 };
 
+            // Define the predicate to check if a number is greater than 10
+            //Predicate<int> isGreaterThanTen = x => x >= 10;
+            Predicate<int> isGreaterThanTen = IsGreaterThanTen;
+
             // THis will return a list of numbers that are 10 an higher
-            List<int> greaterThanEqualTen =  numbers.FindAll(x => x >= 10);
+            List<int> greaterThanEqualTen =  numbers.FindAll(isGreaterThanTen);
             Console.WriteLine("All number 10 or greater: ");
             foreach(int number in greaterThanEqualTen) { Console.WriteLine(number); }
             Console.WriteLine();
+        }
+
+        public static bool IsGreaterThanTen(int x)
+        {
+            return x >= 10;
         }
     }
 }
