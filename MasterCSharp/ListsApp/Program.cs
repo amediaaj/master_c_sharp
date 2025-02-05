2c443488c5ce6bb2c22dfd9e41318dc0b4025601
Alex Amedia <aamedia@asu.edu>
2025-02-05 08:59:22 -0700

    List w/Any

--- MasterCSharp/ListsApp/Program.cs
+++ MasterCSharp/ListsApp/Program.cs
@@ -4,50 +4,18 @@
     {
         static void Main(string[] args)
         {
-            // Creating a list and initializing
-            List<string> colors = ["red", "blue", "green", "red"];
-
-            //List<string> colors = new List<string>
-            //{
-            //    "red",
-            //    "blue",
-            //    "green",
-            //    "red"
-            //};
-
-            foreach (string color in colors)
-            {
-                Console.WriteLine(color);
-            }
-            Console.WriteLine();
-
-            // Rmove() returns bool
-            Console.WriteLine(colors.Remove("red"));
-
-            foreach (string color in colors)
-            {
-                Console.WriteLine(color);
-            }
-            Console.WriteLine();
+        
 
             // Array vs curly brace notation
             List<int> numbers = [10, 5, 15, 3, 9, 25, 18];
-            //List<int> numbers = new List<int> { 10, 5, 15, 3, 9 };
 
-            // Define the predicate to check if a number is greater than 10
-            //Predicate<int> isGreaterThanTen = x => x >= 10;
-            Predicate<int> isGreaterThanTen = IsGreaterThanTen;
-
-            // THis will return a list of numbers that are 10 an higher
-            List<int> greaterThanEqualTen =  numbers.FindAll(isGreaterThanTen);
-            Console.WriteLine("All number 10 or greater: ");
-            foreach(int number in greaterThanEqualTen) { Console.WriteLine(number); }
-            Console.WriteLine();
-        }
-
-        public static bool IsGreaterThanTen(int x)
-        {
-            return x >= 10;
+            if (numbers.Any(x => x > 20))
+            {
+                Console.WriteLine("There are large numbers!");
+            }
+            else {
+                Console.WriteLine("No large numbers!");
+            }
         }
     }
 }
