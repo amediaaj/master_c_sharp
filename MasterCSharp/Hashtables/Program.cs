3bd3a201f004ebeb0754afb2d9c5a65cdf2f5309
Alex Amedia <aamedia@asu.edu>
2025-02-05 12:33:07 -0700

    Legacy Hashtables

---
+++ MasterCSharp/Hashtables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace Hashtables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable studentsTable = new Hashtable();
+
+            Student stud1 = new Student(1, "Maria", 98);
+            Student stud2 = new Student(2, "Jason", 76);
+            Student stud3 = new Student(3, "Clara", 43);
+            Student stud4 = new Student(4, "Steve", 55);
+
+            studentsTable.Add(stud1.Id, stud1);
+            studentsTable.Add(stud2.Id, stud2);
+            studentsTable.Add(stud3.Id, stud3);
+            studentsTable.Add(stud4.Id, stud4);
+
+
+            foreach(DictionaryEntry entry in studentsTable)
+            {
+                Student temp = (Student)entry.Value;
+                Console.WriteLine($"{temp.Id} - {temp.Name} - {temp.GPA}");
+            }
+
+            Console.WriteLine();
+            // or
+
+            foreach (Student value in studentsTable.Values) {
+                Console.WriteLine($"{value.Id} - {value.Name} - {value.GPA}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float GPA { get; set; }
+
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+    }
+}
--- MasterCSharp/ListsApp/Product.cs
+++ MasterCSharp/ListsApp/Product.cs
@@ -7,4 +7,17 @@ namespace ListsApp
         public string Name { get; set; }
         public double Price { get; set; }
     }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float GPA { get; set; }
+
+        public Student(int id, string name, float GPA) {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+    }
 }
