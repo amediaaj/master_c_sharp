ac280d0369f436112e445640cd4bb8a2410d4a31
Alex Amedia <aamedia@asu.edu>
2025-02-06 14:36:29 -0700

    Declaring, Initializing, and Adding Items

---
+++ MasterCSharp/Dictionaries/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+
+namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // key - value
+            // Declaring and initializing a Dictionary
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+
+            employees.Add(101, "John Doe");
+            employees.Add(102, "Bob Smith");
+
+            // access items ini a dictionary
+            string name = employees[101];
+            Console.WriteLine(name);
+            
+        }
+    }
+}
