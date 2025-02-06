6972e5254366f784f44600de20ce53924bbceab6
Alex Amedia <aamedia@asu.edu>
2025-02-06 15:14:04 -0700

    Using a Complex Object as a Value of a Dictionary

--- MasterCSharp/Dictionaries/Program.cs
+++ MasterCSharp/Dictionaries/Program.cs
@@ -6,50 +6,33 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
-
             // key - value
             // Declaring and initializing a Dictionary
-            Dictionary<int, string> employees = new Dictionary<int, string>();
+            Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
 
-            employees.Add(101, "John Doe");
-            employees.Add(102, "Bob Smith");
-            employees.Add(103, "Rob Smith");
-            employees.Add(104, "Flob Smith");
-            employees.Add(105, "Dob Smith");
+            employees.Add(1, new Employee("John Does", 35, 100000));
+            employees.Add(2, new Employee("John Doesnt", 25, 200000));
+            employees.Add(3, new Employee("John Wasnt", 45, 80000));
+            employees.Add(4, new Employee("John Will", 15, 50000));
 
-
-            // access items in a dictionary
-            string name = employees[101];
-            Console.WriteLine(name);
-
-            // update data in a dictionary
-            employees[102] = "Jane Smith";
-
-            //employees.Remove(101);
-            // bool added = employees.TryAdd(102, "Cassidy Hare");
-
-            if(!employees.ContainsKey(104))
+            foreach(var item in employees)
             {
-                employees.Add(104, "Mike Jike");
+                Console.WriteLine($"{item.Key}: {item.Value.Name}");
             }
+        }
+    }
 
-            // Adding by checking if key exists
-            int counter = 101;
-            while (employees.ContainsKey(counter)) {
-                counter++;
-            }
-            employees.Add(counter, "Alex Amedia");
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
 
-
-            // TryAdd with incrementing counter
-            counter = 101;
-            while (!employees.TryAdd(counter++, "Cassidy Hare")) ;
-
-            foreach(KeyValuePair<int, string> kvp in employees)
-            {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-            }
-            
+        public Employee(string name, int age, int salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
         }
     }
 }
