856688ffb434833e052ef74f03d215bebe950b88
Alex Amedia <aamedia@asu.edu>
2025-02-07 08:53:23 -0700

    Managing Multiple Types of Exceptions

--- MasterCSharp/TryCatch/Program.cs
+++ MasterCSharp/TryCatch/Program.cs
@@ -9,10 +9,19 @@ namespace TryCatch
         {
             try
             {
-                double result = ThrowDemo.Divide(1, 0);
+                int denominator = int.Parse(Console.ReadLine());
+                double result = ThrowDemo.Divide(1, denominator);
                 Console.WriteLine(result);
             }
             catch (DivideByZeroException e)
+            {
+                Console.WriteLine("DON'T DIVIDE BY ZERO!!! : " + e.Message);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("THAT NUMBER WAS WAY TOO BIG!!! " + e.Message);
+            }
+            catch (Exception e) 
             {
                 Console.WriteLine("An error occured: " + e.Message);
             }
